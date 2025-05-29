01c4cdb2c15125a0538e8012c7964499f464c75d
aginamena <aginamena5@gmail.com>
2025-05-29 14:25:00 -0400

    added optional requirments

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let video;
 let appInterval = null;
 let firstTime = true;
 function preload() {
-  bodyPose = ml5.bodyPose("BlazePose");
+  bodyPose = ml5.bodyPose("BlazePose", { flipped: true });
 }
 
 function setup() {
   createCanvas(640, 480);
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO, { flipped: true });
   video.size(640, 480);
   video.hide();
   bodyPose.detectStart(video, (result) => (poses = result));
@@ -18,8 +18,20 @@ function setup() {
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
-
+  image(video, 0, 0);
+  //display the vertical lines that will serve as borders to position the viewer
+  stroke(255, 0, 0);
+  strokeWeight(2);
+  line(100, 480, 100, 0);
+  line(540, 480, 540, 0);
+  //display the shoulders keypoints
+  if (poses.length > 0) {
+    const { left_shoulder, right_shoulder } = poses[0];
+    fill(0, 255, 0);
+    noStroke();
+    circle(left_shoulder.x, left_shoulder.y, 10);
+    circle(right_shoulder.x, right_shoulder.y, 10);
+  }
   if (firstTime) {
     firstTime = false;
     appInterval = setInterval(app, 15000);
@@ -48,17 +60,33 @@ function app() {
   if (poses.length > 0 && inFullView()) {
     voicePrompt("Preparing to take your photo. In 5, 4, 3, 2, 1");
     setTimeout(() => {
-      if (inFullView()) {
+      const feedback = getDirectionalFeedback();
+      if (feedback.length > 0) {
+        voicePrompt(feedback);
+      } else if (inFullView()) {
         saveCanvas("pic.jpg");
         voicePrompt("Your photo has been taken and saved to your system");
         clearInterval(appInterval);
-      } else {
-        voicePrompt(
-          "Sorry, I am unable to detect your face and shoulders. Please adjust your position."
-        );
       }
     }, 10000);
   }
+}
+
+function getDirectionalFeedback() {
+  if (poses.length == 0)
+    return "Sorry, I am unable to detect your face and shoulders. Please adjust your position.";
+  const { left_shoulder, right_shoulder } = poses[0];
+  //100px from the left
+  if (left_shoulder.x <= 150)
+    return "Please move slightly to your left to center your body in the camera";
+  //490px from the left
+  else if (right_shoulder.x >= 490)
+    return "Please move slightly to your right to center your body in the camera";
+  return "";
+}
+
+function mousePressed() {
+  console.log(poses[0]);
 }
 
 document.getElementById("retakePhoto").addEventListener("click", () => {
